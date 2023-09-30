d073844f988e0e3e9ee4d2d329393d6ed2231393
letsjo <hyunoh.jo@gmail.com>
2023-10-01 04:19:37 +0900

    Feat: add lottie image

---
+++ components/Animation.js
@@ -0,0 +1,7 @@
+'use client';
+import React from 'react';
+import Lottie from 'react-lottie-player';
+
+export const Animation = ({ ...rest }) => <Lottie loop play {...rest} />;
+
+export default Animation;
