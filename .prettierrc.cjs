b0a3d11602256363ea7379f0753af39b196911a5
letsjo <hyunoh.jo@gmail.com>
2023-09-30 21:23:13 +0900

    vault backup: 2023-09-30 21:23:13 - HYUNOH

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 120,
+  proseWrap: 'never',
+  endOfLine: 'lf',
+  useTabs: false,
+  semi: true,
+  jsxSingleQuote: true,
+  arrowParens: 'always',
+  formatOnSave: true,
+  htmlWhitespaceSensitivity: 'css',
+}
---
+++ components/HeroSection.js
@@ -0,0 +1,51 @@
+'use client';
+import React from 'react';
+import Image from './Image';
+import { TypeAnimation } from 'react-type-animation';
+
+const HeroSection = () => {
+  return (
+    <section className='lg:py-16'>
+      <div className='grid grid-cols-1 sm:grid-cols-12'>
+        <div className='col-span-8 place-self-center justify-self-start text-center sm:text-left'>
+          <h1 className='mb-4 text-3xl font-extrabold text-white sm:text-4xl lg:text-6xl lg:leading-normal'>
+            <span className='bg-gradient-to-r from-primary-400 to-gray-600 bg-clip-text text-transparent'>
+              Hello, I&apos;m{' '}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={['Tummy', 1000, 'Web Developer', 1000, 'Mobile Developer', 1000, 'UI/UX Designer', 1000]}
+              wrapper='span'
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className='mb-6 text-base text-[#ADB7BE] sm:text-lg lg:text-xl'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+          </p>
+          <div>
+            <button className='mr-4 w-full rounded-full bg-gradient-to-br from-primary-500 to-gray-500 px-6 py-3 text-white hover:bg-slate-200 sm:w-fit'>
+              Hire Me
+            </button>
+            <button className='mt-3 w-full rounded-full bg-gradient-to-br from-primary-500 to-gray-500 px-1 py-1 text-white hover:bg-slate-800 sm:w-fit'>
+              <span className='block rounded-full bg-[#121212] px-5 py-2 hover:bg-slate-800'>Download CV</span>
+            </button>
+          </div>
+        </div>
+        <div className='col-span-4 mt-4 place-self-center lg:mt-0'>
+          <div className='relative h-[250px] w-[250px] rounded-full bg-[#181818] lg:h-[400px] lg:w-[400px]'>
+            <Image
+              src='/static/images/hero-image.png'
+              alt='hero image'
+              className='absolute top-1/2 left-1/2'
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
