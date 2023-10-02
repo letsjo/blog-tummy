04e7b7163bde3120d8b79631f57b98e718579834
letsjo <hyunoh.jo@gmail.com>
2023-10-02 16:30:09 +0900

    Chore: install svg component library

--- next.config.js
+++ next.config.js
@@ -67,13 +67,23 @@ module.exports = withBundleAnalyzer({
     ];
   },
   webpack: (config, { dev, isServer }) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: { not: /components/ },
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: /components/,
+        use: ['@svgr/webpack'],
+      },
+    );
 
     if (!dev && !isServer) {
-      // Replace React with Preact only in client production build
       Object.assign(config.resolve.alias, {
         'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
         react: 'preact/compat',
