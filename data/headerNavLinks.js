63d482b2f8c131d70ddbc3318bbbcd932e1b844b
letsjo <hyunoh.jo@gmail.com>
2023-10-03 13:49:02 +0900

    Feat: change header menu

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,8 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
+  { href: '/blog', title: '기록하기' },
   { href: '/tags', title: '태그' },
-  { href: '/projects', title: 'Portfolio' },
-  { href: '/categories/memo', title: 'Memo' },
+  { href: '/projects', title: '포트폴리오' },
+  { href: '/categories/memo', title: '메모' },
 ];
 
 export default headerNavLinks;
