d1ada81f8b33ed5bb682ef9b54f39e763e7f1dfa
letsjo <hyunoh.jo@gmail.com>
2023-10-03 13:19:07 +0900

    Feat: add tags menu

--- components/ProjectTag.js
+++ components/ProjectTag.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const ProjectTag = ({ name, onClick, isSelected }) => {
-  const buttonStyles = isSelected ? 'border-primary-500' : 'text-gray-500 border-slate-500';
+  const buttonStyles = isSelected
+    ? 'border-primary-500'
+    : 'text-gray-500 border-slate-400 hover:border-gray-600 hover:text-gray-800 dark:hover:border-gray-400 dark:hover:text-gray-200';
   return (
     <button
-      className={`${buttonStyles} cursor-pointer rounded-full border-2 px-4 py-2 text-xl hover:text-black dark:hover:text-white md:px-6 md:py-3`}
+      className={`${buttonStyles} cursor-pointer rounded-full border-2 px-4 py-2 text-xl md:px-6 md:py-3`}
       onClick={() => onClick(name)}
     >
       {name}
