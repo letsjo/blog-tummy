582c72196f8e8d00a415a40f9d85cee28aa271c1
letsjo <hyunoh.jo@gmail.com>
2023-10-09 16:19:43 +0900

    Feat: delete tag menu

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,6 @@
 const headerNavLinks = [
   { href: '/projects', title: '포트폴리오' },
   { href: '/blog', title: '개발블로그' },
-  { href: '/tags', title: '관심키워드' },
   { href: '/categories/memo', title: '메모' },
 ];
 
